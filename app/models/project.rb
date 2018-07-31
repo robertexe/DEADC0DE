8b19d82d76efacfefeb0286628db64986f5f976f
robertexe <robtabares92@gmail.com>
2018-07-31 11:14:16 -0400

    established routes and more routes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :language
+  has_many :user_projects
+  has_many :users, through: :user_projects
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,30 @@
-Rails.application.routes.draw do
-  resources :projects, only: [:index]
-  resources :comments
-  resources :languages
-  resources :posts
-  resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  resources :users, only: [:create, :show, :delete]
+  resources :languages, only: [:index]
+  resources :projects
+  resources :posts
+
+
+  resources :users do
+    resources :projects, only: [:index, :create, :delete]
+  end
+
+  resources :languages do
+    resources :projects, only: [:index]
+    resources :posts, only: [:index]
+  end
+
+  resources :posts do
+    resources :comments, only: [:index, :create, :update, :delete]
+  end
 end
+
+
+##  /languages
+## /languages/ids/projects
+## /user/id/projects
+## /project/id
+## /posts
+## /languages/id/posts
+## /posts/id/comments
